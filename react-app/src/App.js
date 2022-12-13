d17412f59f3b1210cc8a3184324836f719eb6147
tamago-boro <ppdayopc@gmail.com>
2022-12-13 23:52:19 +0900

    test

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Rea ct
         </a>
       </header>
     </div>
